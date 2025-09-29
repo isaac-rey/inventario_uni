213cef1a12c556de143f5cf19839b9075b5bb8f6
MYKE002 <114688972+MYKE002@users.noreply.github.com>
2025-09-29 16:10:21 -0300

    base de datos actuaslizado

---
+++ inventario_uni.sql
@@ -0,0 +1,497 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 29-09-2025 a las 21:09:56
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `inventario_uni`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `areas`
+--
+
+CREATE TABLE `areas` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(80) NOT NULL,
+  `creado_en` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `areas`
+--
+
+INSERT INTO `areas` (`id`, `nombre`, `creado_en`) VALUES
+(1, 'Biblioteca', '2025-08-31 16:25:35'),
+(2, 'Secretaría', '2025-08-31 16:25:35'),
+(3, 'Sala Informática', '2025-08-31 16:25:35');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `componentes`
+--
+
+CREATE TABLE `componentes` (
+  `id` int(11) NOT NULL,
+  `equipo_id` int(11) NOT NULL,
+  `tipo` varchar(80) NOT NULL,
+  `marca` varchar(80) DEFAULT NULL,
+  `modelo` varchar(120) DEFAULT NULL,
+  `nro_serie` varchar(120) DEFAULT NULL,
+  `estado` varchar(20) NOT NULL DEFAULT 'bueno',
+  `observacion` text DEFAULT NULL,
+  `creado_en` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `equipos`
+--
+
+CREATE TABLE `equipos` (
+  `id` int(11) NOT NULL,
+  `area_id` int(11) NOT NULL,
+  `sala_id` int(11) DEFAULT NULL,
+  `tipo` varchar(60) NOT NULL,
+  `marca` varchar(80) DEFAULT NULL,
+  `modelo` varchar(120) DEFAULT NULL,
+  `nro_serie` varchar(120) DEFAULT NULL,
+  `serial_interno` varchar(32) NOT NULL,
+  `estado` varchar(20) NOT NULL DEFAULT 'bueno',
+  `prestado` tinyint(1) NOT NULL DEFAULT 0,
+  `con_reporte` tinyint(1) NOT NULL DEFAULT 0,
+  `detalles` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL CHECK (json_valid(`detalles`)),
+  `creado_en` timestamp NOT NULL DEFAULT current_timestamp(),
+  `actualizado_en` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp(),
+  `en_mantenimiento` tinyint(1) DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `equipos`
+--
+
+INSERT INTO `equipos` (`id`, `area_id`, `sala_id`, `tipo`, `marca`, `modelo`, `nro_serie`, `serial_interno`, `estado`, `prestado`, `con_reporte`, `detalles`, `creado_en`, `actualizado_en`, `en_mantenimiento`) VALUES
+(5, 1, 2, 'TV 43', 'tokyo', 'tffe3', NULL, '23b47cd5e388', 'bueno', 0, 0, NULL, '2025-09-29 13:22:56', '2025-09-29 13:39:19', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `estudiantes`
+--
+
+CREATE TABLE `estudiantes` (
+  `id` int(11) NOT NULL,
+  `ci` varchar(20) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `apellido` varchar(100) NOT NULL,
+  `email` varchar(150) NOT NULL,
+  `password_hash` varchar(255) NOT NULL,
+  `creado_en` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `estudiantes`
+--
+
+INSERT INTO `estudiantes` (`id`, `ci`, `nombre`, `apellido`, `email`, `password_hash`, `creado_en`) VALUES
+(5, '4567894', 'hola', 'como estas', 'cualquiercosa@gmail.com', '$2y$10$fR2sLhhixQq/iOjzW4L68eGsbZ4IM/SLEwwxk/qCBQdibG1mcLB/C', '2025-09-29 13:20:38'),
+(6, '7894561', 'prueba1', 'la prueba', 'prueba@gmail.com', '$2y$10$WapLtCIvGwlwrAYHPA8COeZiCRCU6LlF93acNLjwEQ7MpHjmaGhs.', '2025-09-29 13:32:03'),
+(7, '789654123', 'gilberto', 'hola', 'hdjfikjsahdfkjha@gmail.com', '$2y$10$Zw2SMQJDYhm4O1DbOseuve7LLY4x3k/M49te/mEO.MdiPzw51s3Re', '2025-09-29 14:17:50');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `mantenimientos`
+--
+
+CREATE TABLE `mantenimientos` (
+  `id` int(11) NOT NULL,
+  `equipo_id` int(11) NOT NULL,
+  `usuario_id` int(11) DEFAULT NULL,
+  `destino` varchar(255) DEFAULT NULL,
+  `motivo` text DEFAULT NULL,
+  `fecha_envio` datetime NOT NULL,
+  `fecha_devolucion` datetime DEFAULT NULL,
+  `solucionado` tinyint(1) DEFAULT NULL,
+  `observaciones` text DEFAULT NULL,
+  `creado_en` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `password_resets`
+--
+
+CREATE TABLE `password_resets` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `token` varchar(128) NOT NULL,
+  `expires_at` datetime NOT NULL,
+  `used` tinyint(1) DEFAULT 0,
+  `created_at` datetime DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `password_resets`
+--
+
+INSERT INTO `password_resets` (`id`, `user_id`, `token`, `expires_at`, `used`, `created_at`) VALUES
+(1, 5, '53f030eec6cede55ee5dd872f3bb762ff7cf33e0a9d5f8bf10a98833113e0fc9', '2025-09-14 18:32:58', 0, '2025-09-14 13:02:58'),
+(2, 5, 'd1f7170edfbdd455fd0aa76dfb73a6be00498ff89d5abb387ffb9d69a5eccf15', '2025-09-14 18:45:54', 0, '2025-09-14 13:15:54'),
+(3, 5, '75e3edaf10329b5974f052474bd8789324dd3a8a9fbfaa849bbf71441844a14b', '2025-09-15 22:36:46', 0, '2025-09-15 17:06:46'),
+(4, 5, '99d1acacca092867d963f68ad18e9f0fb92e2e26cf8ef3a1195ac73be7f8b029', '2025-09-15 22:37:20', 0, '2025-09-15 17:07:20'),
+(5, 5, 'd2b5ad2e7caa60cbcd0601dd65177db9654773257d4b4ba3038ebe4c179695ad', '2025-09-15 22:49:03', 0, '2025-09-15 17:19:03'),
+(6, 5, 'f508175d9330cbe5cf753d52378c7bf5cbaf960002c830ea01831a1b684a7729', '2025-09-15 23:01:04', 0, '2025-09-15 17:31:04'),
+(7, 5, '0bf21aa776032f264e476671a23d976a83e2d922c19d090e931351879836189b', '2025-09-15 23:04:37', 0, '2025-09-15 17:34:37'),
+(8, 5, '5762ae3f704f96592271b366c73be7e1909a9145724325b20900925b6f159fb3', '2025-09-15 23:15:55', 0, '2025-09-15 17:45:55'),
+(9, 5, 'cd69ad26c21cdbf7d1069347447ae04297201b1e777cc224079582e36e860905', '2025-09-15 23:16:02', 0, '2025-09-15 17:46:02'),
+(14, 5, '77d60ec8e6e7a7104bf7d6a42b667a23cb9435d8defd85ffd0a394a74ba76a45', '2025-09-15 23:25:56', 0, '2025-09-15 17:55:56'),
+(15, 5, '326b95673b3fd2d87180f4f0e58164f21015a8dd14f059b15ce2269b04f1466c', '2025-09-15 23:26:21', 0, '2025-09-15 17:56:21'),
+(26, 7, '1651419053d5a8b5a846fa50fa3af9c92f97caff812d70759843e50ab782514f', '2025-09-29 17:01:20', 1, '2025-09-29 11:31:20');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `prestamos`
+--
+
+CREATE TABLE `prestamos` (
+  `id` int(11) NOT NULL,
+  `equipo_id` int(11) NOT NULL,
+  `estudiante_id` int(11) NOT NULL,
+  `fecha_entrega` datetime NOT NULL DEFAULT current_timestamp(),
+  `fecha_devolucion` datetime DEFAULT NULL,
+  `estado` varchar(20) NOT NULL DEFAULT 'activo',
+  `observacion` text DEFAULT NULL,
+  `creado_en` timestamp NOT NULL DEFAULT current_timestamp(),
+  `motivo_cancelacion` text DEFAULT NULL,
+  `devuelto_por_tercero_nombre` varchar(120) DEFAULT NULL,
+  `devuelto_por_tercero_ci` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `prestamos`
+--
+
+INSERT INTO `prestamos` (`id`, `equipo_id`, `estudiante_id`, `fecha_entrega`, `fecha_devolucion`, `estado`, `observacion`, `creado_en`, `motivo_cancelacion`, `devuelto_por_tercero_nombre`, `devuelto_por_tercero_ci`) VALUES
+(13, 5, 5, '2025-09-29 10:25:22', '2025-09-29 10:25:40', 'devuelto', 'Sala de informática', '2025-09-29 13:25:22', NULL, NULL, NULL),
+(14, 5, 5, '2025-09-29 10:28:22', '2025-09-29 10:39:19', 'devuelto', '', '2025-09-29 13:28:22', NULL, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `reporte_fallos`
+--
+
+CREATE TABLE `reporte_fallos` (
+  `id` int(11) NOT NULL,
+  `fecha` date NOT NULL,
+  `tipo_fallo` varchar(255) NOT NULL,
+  `descripcion_fallo` text NOT NULL,
+  `id_equipo` int(11) NOT NULL,
+  `nombre_usuario_reportante` varchar(120) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `roles`
+--
+
+CREATE TABLE `roles` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `roles`
+--
+
+INSERT INTO `roles` (`id`, `nombre`) VALUES
+(1, 'admin'),
+(2, 'bibliotecaria');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `salas`
+--
+
+CREATE TABLE `salas` (
+  `id` int(11) NOT NULL,
+  `area_id` int(11) NOT NULL,
+  `nombre` varchar(120) NOT NULL,
+  `descripcion` text DEFAULT NULL,
+  `creado_en` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `salas`
+--
+
+INSERT INTO `salas` (`id`, `area_id`, `nombre`, `descripcion`, `creado_en`) VALUES
+(1, 1, 'Estantería A', NULL, '2025-08-31 16:25:36'),
+(2, 1, 'Depósito Biblioteca', NULL, '2025-08-31 16:25:36'),
+(3, 2, 'Mesa Principal', NULL, '2025-08-31 16:25:36'),
+(4, 3, 'Laboratorio 1', NULL, '2025-08-31 16:25:36'),
+(5, 3, 'Laboratorio 2', NULL, '2025-08-31 16:25:36');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `ci` varchar(20) NOT NULL,
+  `email` varchar(150) NOT NULL,
+  `nombre` varchar(120) NOT NULL,
+  `password_hash` varchar(255) NOT NULL,
+  `role_id` int(11) NOT NULL,
+  `creado_en` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `ci`, `email`, `nombre`, `password_hash`, `role_id`, `creado_en`) VALUES
+(5, '123456', 'isaacmiranda290@gmail.com', 'Isaac Miranda', '$2a$12$obdsmKZKP18niFoFF8iG6eV7y6APB2Q3GjQXPdC5dvb5rMKZkwyuu', 2, '2025-09-14 16:01:44'),
+(7, '7400254', 'kevinalegre181@gmail.com', 'kevin', '$2y$10$EfpsXdbiRfwzC1GA6D9NieNfBdXgsZbMwG4SKQsiVslUrjKJjP.ka', 1, '2025-09-19 15:55:41'),
+(10, '5920912', 'perlaj34@gamil.com', 'richar', '$2y$10$ZSCq4gccx6biyaA1DIzR2eoWYX5MtoIjLAsiaqfBP6/uucowyKnBy', 2, '2025-09-29 14:43:29');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `areas`
+--
+ALTER TABLE `areas`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `nombre` (`nombre`);
+
+--
+-- Indices de la tabla `componentes`
+--
+ALTER TABLE `componentes`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idx_equipo_tipo` (`equipo_id`,`tipo`);
+
+--
+-- Indices de la tabla `equipos`
+--
+ALTER TABLE `equipos`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `unique_serial_interno` (`serial_interno`),
+  ADD KEY `fk_equipos_sala` (`sala_id`),
+  ADD KEY `idx_tipo_estado` (`tipo`,`estado`),
+  ADD KEY `idx_area_sala` (`area_id`,`sala_id`);
+
+--
+-- Indices de la tabla `estudiantes`
+--
+ALTER TABLE `estudiantes`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `ci` (`ci`);
+
+--
+-- Indices de la tabla `mantenimientos`
+--
+ALTER TABLE `mantenimientos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idx_equipo` (`equipo_id`);
+
+--
+-- Indices de la tabla `password_resets`
+--
+ALTER TABLE `password_resets`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `token` (`token`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indices de la tabla `prestamos`
+--
+ALTER TABLE `prestamos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idx_equipo_estado` (`equipo_id`,`estado`),
+  ADD KEY `idx_estudiante` (`estudiante_id`);
+
+--
+-- Indices de la tabla `reporte_fallos`
+--
+ALTER TABLE `reporte_fallos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_equipo` (`id_equipo`);
+
+--
+-- Indices de la tabla `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `nombre` (`nombre`);
+
+--
+-- Indices de la tabla `salas`
+--
+ALTER TABLE `salas`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `unique_area_sala` (`area_id`,`nombre`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `ci` (`ci`),
+  ADD KEY `role_id` (`role_id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `areas`
+--
+ALTER TABLE `areas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `componentes`
+--
+ALTER TABLE `componentes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `equipos`
+--
+ALTER TABLE `equipos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de la tabla `estudiantes`
+--
+ALTER TABLE `estudiantes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT de la tabla `mantenimientos`
+--
+ALTER TABLE `mantenimientos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT de la tabla `password_resets`
+--
+ALTER TABLE `password_resets`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+
+--
+-- AUTO_INCREMENT de la tabla `prestamos`
+--
+ALTER TABLE `prestamos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT de la tabla `reporte_fallos`
+--
+ALTER TABLE `reporte_fallos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `roles`
+--
+ALTER TABLE `roles`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `salas`
+--
+ALTER TABLE `salas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `componentes`
+--
+ALTER TABLE `componentes`
+  ADD CONSTRAINT `fk_componentes_equipo` FOREIGN KEY (`equipo_id`) REFERENCES `equipos` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `equipos`
+--
+ALTER TABLE `equipos`
+  ADD CONSTRAINT `fk_equipos_area` FOREIGN KEY (`area_id`) REFERENCES `areas` (`id`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_equipos_sala` FOREIGN KEY (`sala_id`) REFERENCES `salas` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `mantenimientos`
+--
+ALTER TABLE `mantenimientos`
+  ADD CONSTRAINT `fk_mantenimientos_equipo` FOREIGN KEY (`equipo_id`) REFERENCES `equipos` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `password_resets`
+--
+ALTER TABLE `password_resets`
+  ADD CONSTRAINT `password_resets_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE;
+
+--
+-- Filtros para la tabla `prestamos`
+--
+ALTER TABLE `prestamos`
+  ADD CONSTRAINT `fk_prestamo_equipo` FOREIGN KEY (`equipo_id`) REFERENCES `equipos` (`id`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_prestamo_est` FOREIGN KEY (`estudiante_id`) REFERENCES `estudiantes` (`id`) ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `reporte_fallos`
+--
+ALTER TABLE `reporte_fallos`
+  ADD CONSTRAINT `reporte_fallos_ibfk_1` FOREIGN KEY (`id_equipo`) REFERENCES `equipos` (`id`);
+
+--
+-- Filtros para la tabla `salas`
+--
+ALTER TABLE `salas`
+  ADD CONSTRAINT `fk_salas_area` FOREIGN KEY (`area_id`) REFERENCES `areas` (`id`) ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD CONSTRAINT `usuarios_ibfk_1` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
